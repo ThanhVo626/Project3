a0c734b827add644fdac1f081898a3074269728f
sportsrock <114627777+sportsrock@users.noreply.github.com>
2023-10-05 19:16:21 -0400

    Add files via upload

---
+++ project 3.sql
@@ -0,0 +1,19 @@
+CREATE TABLE Zipcode (
+    DataYear INT,
+    DashboardFuelTypeGroup VARCHAR(255),
+    FuelType VARCHAR(255),
+    Zip VARCHAR(10),
+    NumberOfVehicles INT
+);
+
+CREATE TABLE Electricvehicles (
+    DataYear INT,
+    County VARCHAR(255),
+    DashboardFuelTypeGroup VARCHAR(255),
+    FuelType VARCHAR(255),
+    Manufacturer VARCHAR(255),
+    Model VARCHAR(255),
+    NumberOfVehicles INT
+);
+
+
