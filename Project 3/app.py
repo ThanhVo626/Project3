53968920afbfce8c736cb27c782191b25d08b846
ThanhVo626 <134254048+ThanhVo626@users.noreply.github.com>
2023-10-10 13:27:21 -0400

    Add files via upload

--- Project 3/app.py
+++ Project 3/app.py
@@ -11,7 +11,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
 import flask
-from flask import Flask,jsonify, render_template
+from flask import Flask,jsonify, render_template,json
 
 engine = create_engine("postgresql+psycopg2://postgres:OmamoriHimari9@localhost:5432/plspls")
 
@@ -23,6 +23,27 @@ Base.prepare(autoload_with=engine)
 # Save reference to the table
 vehicles = Base.classes.electricvehicles
 
+session = Session(engine)
+
+
+    # Query all info
+results = session.query(vehicles.datayear, vehicles.county, vehicles.dashboardfueltypegroup,
+                            vehicles.fueltype,vehicles.manufacturer,vehicles.model,vehicles.numberofvehicles).all()
+
+session.close()
+
+all_vehicles = []
+for datayear,county,dashboardfueltypegroup,fueltype,manufacturer,model,numberofvehicles in results:
+    vehicles_dict={}
+    vehicles_dict["datayear"] = datayear
+    vehicles_dict['county'] = county
+    vehicles_dict['dashboardfueltypegroup'] = dashboardfueltypegroup
+    vehicles_dict['fueltype'] = fueltype
+    vehicles_dict['manufacturer'] = manufacturer
+    vehicles_dict['model'] = model
+    vehicles_dict['numberofvehicles'] = numberofvehicles
+    all_vehicles.append(vehicles_dict)
+
 #################################################
 # Flask Setup
 #################################################
@@ -32,42 +53,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html',all_vehicles = all_vehicles)
     
 
 @app.route('/Overall')
 def overall ():
-    session = Session(engine)
 
-    """Return a list of passenger data including the name, age, and sex of each passenger"""
-    # Query all passengers
-    results = session.query(vehicles.datayear, vehicles.county, vehicles.dashboardfueltypegroup,
-                            vehicles.fueltype,vehicles.manufacturer,vehicles.model,vehicles.numberofvehicles).all()
-
-    session.close()
-
-    all_vehicles = []
-    for datayear,county,dashboardfueltypegroup,fueltype,manufacturer,model,numberofvehicles in results:
-        vehicles_dict={}
-        vehicles_dict["datayear"] = datayear
-        vehicles_dict['county'] = county
-        vehicles_dict['dashboardfueltypegroup'] = dashboardfueltypegroup
-        vehicles_dict['fueltype'] = fueltype
-        vehicles_dict['manufacturer'] = manufacturer
-        vehicles_dict['model'] = model
-        vehicles_dict['numberofvehicles'] = numberofvehicles
-        all_vehicles.append(vehicles_dict)
-    
-    countys = [x['county'] for x in all_vehicles]
-    
-    label = []
-    value = []
-    for x in countys :
-        if x not in label:
-            label.append(x)
-            value.append(countys.count(x))
-
-    return render_template('overall.html')
+    return jsonify(all_vehicles)
 
 @app.route('/players')
 def players ():
--- Project 3/static/js/myJS.js
+++ Project 3/static/js/myJS.js
@@ -1,57 +1,268 @@
+var vehicles = JSON.parse(test)
+
+label = []
+value = []
+
+//Plot 1 vehicle by year
+
+for (let i =0; i < vehicles.length; i++){
+  row = vehicles[i]
+  if (label.includes(row.datayear)) {
+  }
+  else {
+    label.push(row.datayear)
+    count=0
+    for (let i =0; i < vehicles.length; i++){
+      row2 = vehicles[i]
+      if (row.datayear == row2.datayear){
+        count += row2.numberofvehicles
+      }
+    }
+    value.push(count)
+  }};
+
+let data = [{
+  y: value,
+  x: label,
+  type: 'line'  
+}]
 var layout = {
-  title: "Something",
+  title: {text:"Total Vehicles by Year",font:{color:'#FFFFFF'}},
   autosize : false,
-  width:1000,
   height:500,
+  width:1200,
   paper_bgcolor: '#2a3036',
   plot_bgcolor: '#2a3036',
   showlegned:true,
-  xaxis: {color: '#FFFFFF'
-
+  xaxis: {color: '#FFFFFF',title:'Year'
   },
   yaxis:{color: '#FFFFFF'
+  }};
+Plotly.newPlot('plot',data,layout);
 
-  }
+// Vehicle by county
 
-};
-
-
-// Initializes the page with a default plot
-function init() {
-    data = [{
-      x: [1, 2, 3, 4, 5],
-      y: [1, 2, 4, 8, 16] }];
-  
-    Plotly.newPlot("plot", data,layout);
-  }
-  
-  // Call updatePlotly() when a change takes place to the DOM
-  d3.selectAll("#selDataset").on("change", updatePlotly);
-  
-  // This function is called when a dropdown menu item is selected
-  function updatePlotly() {
-    // Use D3 to select the dropdown menu
-    let dropdownMenu = d3.select("#selDataset");
-    // Assign the value of the dropdown menu option to a variable
-    let dataset = dropdownMenu.property("value");
-  
-    // Initialize x and y arrays
-    let x = [];
-    let y = [];
-  
-    if (dataset === 'g1') {
-      x = [1, 2, 3, 4, 5];
-      y = [1, 2, 4, 8, 16];
+function yearPlot2(dataset){
+label = []
+value = []
+for (let i =0; i < vehicles.length; i++){
+  row = vehicles[i]
+  if (row.datayear == dataset){
+    if (label.includes(row.county)) {
     }
+    else {
+      label.push(row.county)
+      count=0
+      for (let i =0; i < vehicles.length; i++){
+        row2 = vehicles[i]
+        if (row.county == row2.county && row2.datayear == dataset){
+          count += row2.numberofvehicles
+        }
+      }
+    value.push(count)
+}}}};
+
+  function init2() {
+    yearPlot2(2022);
+    data2 = [{
+      x: label,
+      y: value,
+      type:'bar'}];
   
-    else if (dataset === 'g2') {
-      x = [10, 20, 30, 40, 50];
-      y = [1, 10, 100, 1000, 10000];
-    }
+      var layout2 = {
+        title: {text:"Total Vehicles by County",font:{color:'#FFFFFF'}},
+        height:500,
+        width:1200,
+        autosize : false,
+        paper_bgcolor: '#2a3036',
+        plot_bgcolor: '#2a3036',
+        showlegned:true,
+        xaxis: {color: '#FFFFFF',title:'County'
+        },
+        yaxis:{color: '#FFFFFF'
+        }};
   
-    // Note the extra brackets around 'x' and 'y'
-    Plotly.restyle("plot", "x", [x]);
-    Plotly.restyle("plot", "y", [y]);
+    Plotly.newPlot("plot2", data2,layout2);
   }
   
-  init();
+ 
+init2();
+
+
+
+//Fuel Type
+function yearPlot3(dataset){
+
+label = []
+value = []
+for (let i =0; i < vehicles.length; i++){
+  row = vehicles[i]
+  if (row.datayear == dataset){
+    if (label.includes(row.fueltype)) {
+    }
+    else {
+      label.push(row.fueltype)
+      count=0
+      for (let i =0; i < vehicles.length; i++){
+        row2 = vehicles[i]
+        if (row.fueltype == row2.fueltype && row2.datayear == dataset){
+          count += row2.numberofvehicles
+        }
+      }
+      value.push(count)
+}}}};
+
+function init3(){
+  yearPlot3(2022)
+  let data3 = [{
+    values: value,
+    labels: label,
+    type: 'pie'  
+  }]
+  var layout3 = {
+    title: {text:"Fuel Type of Vehicles",font:{color:'#FFFFFF'}},
+    height:400,
+    width:700,
+    autosize : false,
+    paper_bgcolor: '#2a3036',
+    plot_bgcolor: '#2a3036',
+    showlegend:true,
+    legend:{font:{color: '#FFFFFF'}},
+    xaxis: {color: '#FFFFFF'
+    },
+    yaxis:{color: '#FFFFFF'
+    }};
+  
+  Plotly.newPlot('plot3',data3,layout3);
+}
+init3();
+
+// Manufacturer
+
+function year4plot4(dataset){
+  label = []
+  value = []
+  for (let i =0; i < vehicles.length; i++){
+    row = vehicles[i]
+    if (row.datayear == dataset){
+      if (label.includes(row.manufacturer)) {
+      }
+      else {
+        label.push(row.manufacturer)
+        count=0
+        for (let i =0; i < vehicles.length; i++){
+          row2 = vehicles[i]
+          if (row.manufacturer == row2.manufacturer && row2.datayear == dataset){
+            count += row2.numberofvehicles
+          }
+        }
+        value.push(count)
+  }}}};
+
+function init4() {
+  year4plot4(2022);
+  data4 = [{
+    x: label,
+    y: value,
+    type:'bar'}];
+
+  var layout4 = {
+  title: {text:"Total Vehicles by Manufacturer",font:{color:'#FFFFFF'}},
+  height:500,
+  width:1200,
+  autosize : false,
+  paper_bgcolor: '#2a3036',
+  plot_bgcolor: '#2a3036',
+  showlegned:true,
+  xaxis: {color: '#FFFFFF',title:'Manufacturer'
+  },
+  yaxis:{color: '#FFFFFF'
+  }};
+
+  Plotly.newPlot("plot4", data4,layout4);
+}
+init4();
+
+//Model
+function yearPlot5(dataset){
+  label = []
+  value = []
+  
+  for (let i =0; i < vehicles.length; i++){
+    row = vehicles[i]
+    if (row.datayear == dataset) {
+      if (label.includes(row.model)) {
+      }
+      else {
+        label.push(row.model)
+        count=0
+        for (let i =0; i < vehicles.length; i++){
+          row2 = vehicles[i]
+          if (row.model == row2.model && row2.datayear == dataset){
+            count += row2.numberofvehicles
+          }
+        }
+      value.push(count)
+  }}}};
+  
+  function init5(){
+    yearPlot5(2022);
+    let data5 = [{
+      values: value,
+      labels: label,
+      type: 'pie'  
+    }]
+    var layout5 = {
+      title: {text:"Models of Vehicles",font:{color:'#FFFFFF'}},
+      height:400,
+      width:700,
+      autosize : false,
+      paper_bgcolor: '#2a3036',
+      plot_bgcolor: '#2a3036',
+      legend:{font:{color: '#FFFFFF'}},
+      showlegend:true,
+    };
+    Plotly.newPlot('plot5',data5,layout5);
+    }
+  init5();
+// Call updatePlotly() when a change takes place to the DOM
+d3.selectAll("#selDataset").on("change", updatePlotly);
+
+// This function is called when a dropdown menu item is selected
+function updatePlotly() {
+  // Use D3 to select the dropdown menu
+  let dropdownMenu = d3.select("#selDataset");
+  // Assign the value of the dropdown menu option to a variable
+  let dataset = dropdownMenu.property("value");
+
+  // Initialize x and y arrays
+  let x = [];
+  let y = [];
+  for (let j = 2010; j < 2023 ; j++){
+    if (dataset == j) {
+      year4plot4(j);
+      x = label;
+      y = value;
+      Plotly.restyle("plot4", "x", [x]);
+      Plotly.restyle("plot4", "y", [y]);
+      yearPlot5(j);
+      labels = label;
+      values = value;
+      Plotly.restyle("plot5", 'labels', [labels]);
+      Plotly.restyle("plot5", 'values', [values]);
+      yearPlot2(j);
+      x = label;
+      y = value;
+      Plotly.restyle("plot2", "x", [x]);
+      Plotly.restyle("plot2", "y", [y]);
+      yearPlot3(j);
+      labels = label;
+      values = value;
+      Plotly.restyle("plot3", 'labels', [labels]);
+      Plotly.restyle("plot3", 'values', [values]);
+    }
+    };
+}
+
+
+
